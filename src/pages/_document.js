8fb3dc8121d746c6b71631df8fcedf8a1b2eb76f
winchester-cell <manili.mm91@gmail.com>
2025-05-18 13:21:38 +0330

    start

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en" className="">
       <Head />
-      <body>
+      <body className="bg-[var(--colBg)]">
         <Main />
         <NextScript />
       </body>

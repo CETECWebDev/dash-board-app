6fcfe0c6657bfb86eb7c77b67e043614cb1e1847
winchester-cell <manili.mm91@gmail.com>
2025-05-18 13:26:08 +0330

    start

---
+++ src/components/module/SideBar.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SideBar() {
+  return (
+    <div className='fixed left-0 top-0 h-screen w-[300px] bg-[var(--colCard)] shadow-md'>SideBar</div>
+  )
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,11 @@
+import SideBar from "@/components/module/SideBar";
 import "@/styles/globals.css";
 import { ThemeProvider } from "next-themes";
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider attribute={'class'} defaultTheme="light" >
+      <SideBar />
       <Component {...pageProps} />
     </ThemeProvider>
   )

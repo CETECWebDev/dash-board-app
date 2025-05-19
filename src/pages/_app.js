7f47e631f4da8b282e12da5342db17be3be72301
winchester-cell <manili.mm91@gmail.com>
2025-05-19 11:35:14 +0330

    fixed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,7 +7,7 @@ import { ThemeProvider } from "next-themes";
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute={'class'} defaultTheme="dark" >
+    <ThemeProvider attribute={'class'} defaultTheme="light" >
       <SideBar />
       <div className="ml-[300px] w-[calc(100%-300px)]">
         <Component {...pageProps} />

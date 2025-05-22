fcba1b3146529849f1f6b49ab92d70fda9e309f4
Ali-Asadian-456 <142652407+Ali-Asadian-456@users.noreply.github.com>
2025-05-23 00:51:40 +0330

    iranYekan font added

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -4,7 +4,7 @@ export default function MainLayout({ children }) {
   const { dir } = useDirectionContext();
 
   return (
-    <div className={`flex ${dir === 'rtl' ? 'font-IranSans' : 'font-roboto'} `}>
+    <div className={`flex ${dir === 'rtl' ? 'font-IranYekan' : 'font-roboto'} `}>
       <div className={`${dir === 'rtl' ? 'lg:mr-[300px]' : 'lg:ml-[300px]'} w-screen lg:w-[calc(100%-300px)] `}>
         {children}
       </div>

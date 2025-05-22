bfc6d2e0e699d45fb66050214a53ba4c530f519a
Ali-Asadian-456 <142652407+Ali-Asadian-456@users.noreply.github.com>
2025-05-22 10:47:16 +0330

    farsi ..

--- src/components/template/Devices.js
+++ src/components/template/Devices.js
@@ -4,16 +4,19 @@ import usePagination from '@/hooks/usePagination';
 import DeviceInfoCard from '../module/DeviceInfoCard';
 import Link from 'next/link';
 import PaginationControls from '../module/PaginationControls';
+import { useDirectionContext } from "@/context/DirectionContext";
+
 
 export default function Devices() {
 
   const devices = mockDevices;
   const [totalPages , pagedList , currentPage , setCurrentPage] = usePagination( 8 , devices)
+  const { dir, toggleDirection } = useDirectionContext();
 
   return (
     <div className="p-5 space-y-4">
 
-      <Link href={'/add-device'} className='  border-2 rounded-full py-2 px-4 border-[var(--colTextA)] hover:text-[var(--textHover)] hover:border-[var(--textHover)]'>+ Add new device</Link>
+      <Link href={'/add-device'} className='  border-2 rounded-full py-2 px-4 border-[var(--colTextA)] hover:text-[var(--textHover)] hover:border-[var(--textHover)]'> {(dir === "ltr" ? "+ Add Device " : " افزودن دستگاه + ")} </Link>
 
       {/* Cards */}
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">

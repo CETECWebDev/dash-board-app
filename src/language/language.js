3b545eeac0f866a95d915f9bd8bd05c8209cab0e
Ali-Asadian-456 <142652407+Ali-Asadian-456@users.noreply.github.com>
2025-05-24 13:08:45 +0330

    language problem fixed

--- src/language/language.js
+++ src/language/language.js
@@ -10,8 +10,6 @@ const languagesText = {
     },
 
   pagination: {
-    // previous: { en: "← ", fa: " ←" },
-    // next: { en: "Next", fa: "بعدی" },
     page: { en: "Page", fa: "صفحه" },
   },
 
@@ -60,43 +58,18 @@ const languagesText = {
         back: { en: 'back', fa: 'بازگشت' }
     },
 
-    pagination: {
-        previous: { en: "← Previous", fa: " قبلی ←" },
-        next: { en: "Next", fa: "بعدی" },
-    },
+ 
 
-    devicespage: {
-        adddevice: { en: " Add new device", fa: " افزودن دستگاه جدید " },
-        settings: { en: "Change Settings", fa: "تغییر تنظیمات" },
-    },
+
     employeespage: {
         adduser: { en: " Add User", fa: "افزودن کاربر  " },
     },
-    pagination: {
-        previous: { en: "← Previous", fa: " قبلی ←" },
-        next: { en: "Next", fa: "بعدی" },
-    },
+   
 
-    devicespage: {
-        adddevice: { en: " Add new device", fa: " افزودن دستگاه جدید " },
-        settings: { en: "Change Settings", fa: "تغییر تنظیمات" },
-    },
-    employeespage: {
-        adduser: { en: " Add User", fa: "افزودن کاربر  " },
-        removemodal: { en: "Are you sure you want to delete this user ?", fa: "آیا مطمئن هستید که می خواهید این کارمند را حذف کنید؟" },
-        removemodal_yes: { en: "Confirm", fa: "تایید" },
-        removemodal_no: { en: "Cancel", fa: "لغو" },
 
-    },
 
-    deviceDataCard: {
-        status: { en: 'Status', fa: 'وضعیت' },
-        active: { en: 'Active', fa: 'فعال' },
-        deactive: { en: 'Deactive', fa: 'غیر فعال' },
-        countedVehicle: { en: 'Counted Vehicle', fa: 'خودرو های شمارش شده' },
-        direction: { en: 'Directions', fa: 'مسیریابی' },
-        detail: { en: ' Details', fa: ' جزئیات' },
-    },
+
+   
 
     deviceSetting: {
         title: {en: 'Device Settings' , fa: 'تنظیمات دستگاه'},
@@ -108,10 +81,6 @@ const languagesText = {
         applyAll: {en: 'Apply to all Devices' , fa: 'اعمال برای همه دستگاها'},
         applySelected: {en: 'Apply to selected Devices' , fa: 'اعمال به دستگاهای انتخاب شده'},
     },
-
-    roads: {
-        addRoad: {en: 'Add Road' , fa: 'افزودن مسیر'}
-    }
 
 
 };

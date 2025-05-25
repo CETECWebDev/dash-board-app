fa6d4e3565a560d680b070523b41d36c900c8af7
Ali-Asadian-456 <142652407+Ali-Asadian-456@users.noreply.github.com>
2025-05-25 13:56:05 +0330

    add road added

--- src/components/template/Roads/Roads.js
+++ src/components/template/Roads/Roads.js
@@ -2,16 +2,16 @@ import React from 'react'
 import RoadsInfoCard from '@/components/module/Cards/RoadsInfoCard';
 import usePagination from '@/hooks/usePagination';
 
-import {mockRoutes} from '@/data/db';
+import {mockRoads} from '@/data/db';
 import PaginationControls from '../../module/Controls/PaginationControls';
 import Link from 'next/link';
 import { useDirectionContext } from "@/context/DirectionContext";
 import { translate } from '@/language/language';
 
 
-export default function Routes() {
-  const Routes = mockRoutes;
-  const [totalPages , pagedList , currentPage , setCurrentPage] = usePagination( 8 , Routes)
+export default function Roads() {
+  const Roads = mockRoads;
+  const [totalPages , pagedList , currentPage , setCurrentPage] = usePagination( 8 , Roads)
   const { dir } = useDirectionContext();
   
   
@@ -21,7 +21,7 @@ export default function Routes() {
 
         {/* Add Device Button */}
       <div className='flex items-center gap-5 mb-3' >
-      <Link href={'#'} className='border-2 rounded-full py-2 px-4 border-[var(--colTextA)] hover:text-[var(--textHover)] hover:border-[var(--textHover)]'> {translate(dir,"Roads.addRoad")} </Link>
+      <Link href={'/add-road'} className='border-2 rounded-full py-2 px-4 border-[var(--colTextA)] hover:text-[var(--textHover)] hover:border-[var(--textHover)]'> {translate(dir,"addRoad.title")} </Link>
       </div>
       {/* Cards */}
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
--- src/language/language.js
+++ src/language/language.js
@@ -1,3 +1,5 @@
+import { TbAxisY } from "react-icons/tb";
+
 const languagesText = {
     sidebar: {
         navigation: { en: 'Navigation Menu', fa: 'منوی ناوبری' },
@@ -35,8 +37,13 @@ const languagesText = {
         detail: { en: ' Details', fa: ' جزئیات' },
     },
 
-    Roads: {
-        addRoad: { en: " Add Road", fa: "افزودن محور " },
+    addRoad: {
+        title: { en: " Add Road", fa: "افزودن محور " },
+        name: { en: "Road Name", fa: "نام محور" },
+        address: { en: "Axis Direction", fa: "ادرس محور" },
+        officeProvince: { en: "Office + Province", fa: "اداره + استان" },
+        click: { en: " add Road", fa: "افزودن محور" },
+        back: { en: "Back", fa: "بازگشت" },
 
     },
 
